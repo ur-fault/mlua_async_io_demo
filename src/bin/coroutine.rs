c2b50c84d11732151b537dc0f42965295c814103
ur-fault <minefuf.fufik@gmail.com>
2025-09-23 01:27:31 +0200

    Add: wrap_blocking() and async sleep in lua

--- src/bin/coroutine.rs
+++ src/bin/coroutine.rs
@@ -2,23 +2,23 @@ use std::{sync::mpsc::channel, thread};
 
 use mlua::{FromLua, Function, Lua, Thread, UserData};
 
+#[derive(Clone)]
+struct EventLoop {
+    to_wake: std::sync::mpsc::Sender<Thread>,
+}
+
+impl UserData for EventLoop {}
+
+impl FromLua for EventLoop {
+    fn from_lua(value: mlua::Value, _: &Lua) -> mlua::Result<Self> {
+        Ok(value.as_userdata().unwrap().borrow::<Self>()?.clone())
+    }
+}
+
 fn main() {
     let lua = Lua::new();
 
     let (send, recv) = channel();
-
-    #[derive(Clone)]
-    struct EventLoop {
-        to_wake: std::sync::mpsc::Sender<Thread>,
-    }
-
-    impl UserData for EventLoop {}
-
-    impl FromLua for EventLoop {
-        fn from_lua(value: mlua::Value, _: &Lua) -> mlua::Result<Self> {
-            Ok(value.as_userdata().unwrap().borrow::<Self>()?.clone())
-        }
-    }
 
     let ev = EventLoop { to_wake: send };
 
@@ -28,22 +28,13 @@ fn main() {
         .create_async_function(async move |l, path: String| {
             let ev = l.named_registry_value::<EventLoop>("ev").unwrap();
             let current_thread = l.current_thread();
-            println!("1");
 
-            let handle = thread::spawn(move || -> Result<_, mlua::Error> {
-                let s = std::fs::read_to_string(&path).map_err(mlua::Error::external)?;
-                ev.to_wake.send(current_thread).unwrap();
-                Ok(s)
-            });
+            let s: String = wrap_blocking(&ev, current_thread, move || {
+                std::fs::read_to_string(&path).map_err(mlua::Error::external)
+            })
+            .await?;
 
-            while !handle.is_finished() {
-                println!("2");
-                let _: () = l.yield_with(()).await?;
-            }
-
-            println!("3");
-
-            Ok(handle.join().unwrap()?)
+            Ok(s)
         })
         .unwrap();
 
@@ -61,11 +52,26 @@ fn main() {
         })
         .unwrap();
 
+    let sleep = lua
+        .create_async_function(async move |l, s: f32| {
+            let ev = l.named_registry_value::<EventLoop>("ev").unwrap();
+            let current_thread = l.current_thread();
+
+            wrap_blocking(&ev, current_thread, move || {
+                std::thread::sleep(std::time::Duration::from_secs_f32(s));
+            })
+            .await;
+
+            Ok(())
+        })
+        .unwrap();
+
     lua.globals()
         .set("read_file_async", read_file_async)
         .unwrap();
     lua.globals().set("exit_game", exit_game).unwrap();
     lua.globals().set("async", spawn_async).unwrap();
+    lua.globals().set("sleep", sleep).unwrap();
     // lua.globals().set("print", print).unwrap();
 
     let lua_code = r#"
@@ -74,6 +80,14 @@ fn main() {
             print("4")
             print(content)
         end)
+
+        for i = 1, 10 do
+            async(function()
+                print("Hello from coroutine " .. i)
+                sleep(1.0)
+                print("Coroutine " .. i .. " woke up after 1 second")
+            end)
+        end
     "#;
 
     lua.load(lua_code).exec().unwrap();
@@ -87,4 +101,23 @@ fn main() {
         // simulate frame time
         std::thread::sleep(std::time::Duration::from_millis(16));
     }
+}
+
+async fn wrap_blocking<'a, T: Send + 'static>(
+    ev: &'a EventLoop,
+    current_thread: Thread,
+    f: impl FnOnce() -> T + Send + 'static,
+) -> T {
+    let ev = ev.clone();
+    let handle = thread::spawn(move || {
+        let result = f();
+        ev.to_wake.send(current_thread).unwrap();
+        result
+    });
+
+    while !handle.is_finished() {
+        let _: () = Lua::new().yield_with(()).await.unwrap();
+    }
+
+    handle.join().unwrap()
 }
