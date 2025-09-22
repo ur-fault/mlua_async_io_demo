5f95d7ff1abdb49d33110ed21c9bf6fc90dc90f8
ur-fault <minefuf.fufik@gmail.com>
2025-09-22 17:39:39 +0200

    Add: basic coroutine based API

---
+++ src/bin/coroutine.rs
@@ -0,0 +1,112 @@
+use std::{sync::mpsc::channel, thread};
+
+use mlua::{FromLua, Function, IntoLuaMulti, Lua, MultiValue, Thread, UserData};
+
+fn main() {
+    let lua = Lua::new();
+
+    let (send, recv) = channel();
+
+    // type Callback = Box<dyn FnOnce(&Lua) -> mlua::Result<()> + Send>;
+    type IoThead = thread::JoinHandle<Result<(), mlua::Error>>;
+
+    #[derive(Clone)]
+    struct EventLoop {
+        to_wake: std::sync::mpsc::Sender<Thread>,
+        // threads: Arc<Mutex<Vec<IoThead>>>,
+    }
+
+    impl UserData for EventLoop {}
+
+    impl FromLua for EventLoop {
+        fn from_lua(value: mlua::Value, _: &Lua) -> mlua::Result<Self> {
+            Ok(value.as_userdata().unwrap().borrow::<Self>()?.clone())
+        }
+    }
+
+    let ev = EventLoop {
+        to_wake: send,
+        // threads: Arc::default(),
+    };
+
+    lua.set_named_registry_value("ev", ev.clone()).unwrap();
+
+    let read_file_async = lua
+        .create_async_function(async move |l, path: String| {
+            let ev = l.named_registry_value::<EventLoop>("ev").unwrap();
+            let current_thread = l.current_thread();
+            let l1 = l.clone();
+
+            println!("1");
+
+            let handle = thread::spawn(move || -> Result<_, mlua::Error> {
+                let s = std::fs::read_to_string(&path).map_err(mlua::Error::external)?;
+                ev.to_wake
+                    .send(current_thread)
+                    .unwrap();
+                Ok(s)
+            });
+
+            while !handle.is_finished() {
+                println!("2");
+                let _: () = l.yield_with(()).await?;
+            }
+
+            println!("3");
+
+            Ok(handle.join().unwrap()?)
+        })
+        .unwrap();
+
+    let exit_game = lua
+        .create_function(|_, code: i32| -> Result<(), mlua::Error> {
+            std::process::exit(code);
+        })
+        .unwrap();
+
+    let spawn_async = lua
+        .create_function(|l, f: Function| -> Result<(), mlua::Error> {
+            let thread = l.create_thread(f)?;
+            let _: () = thread.resume(())?;
+            Ok(())
+        })
+        .unwrap();
+
+    lua.globals()
+        .set("read_file_async", read_file_async)
+        .unwrap();
+    lua.globals().set("exit_game", exit_game).unwrap();
+    lua.globals().set("async", spawn_async).unwrap();
+    // lua.globals().set("print", print).unwrap();
+
+    let lua_code = r#"
+        async(function()
+            content = read_file_async("Cargo.toml")
+            print("4")
+            print(content)
+        end)
+    "#;
+
+    lua.load(lua_code).exec().unwrap();
+
+    // main game loop
+    loop {
+        // process all callbacks
+        while let Ok(to_wake) = recv.try_recv() {
+            let _: () = to_wake.resume(()).unwrap();
+        }
+
+        // let mut lock = ev.threads.lock().unwrap();
+        // for i in (0..lock.len()).rev() {
+        //     println!("Checking thread {}", i);
+        //     if lock[i].is_finished() {
+        //         let th = lock.remove(i);
+        //         th.join().unwrap().unwrap();
+        //     }
+        // }
+        // drop(lock);
+
+        // simulate frame time
+        std::thread::sleep(std::time::Duration::from_millis(16));
+    }
+}
